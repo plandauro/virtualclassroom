4f2f010876aa1c246da8ef6883003b31278a1afd
Pedro <plandauro@unab.edu.pe>
2021-11-30 16:54:40 -0500

    mitad del video 45-FALTA

---
+++ app/Observers/LessonObserver.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Lesson;
+
+class LessonObserver
+{
+    //Observer
+    public function creating(Lesson $lesson){
+        $url = $lesson->url;
+        $platform_id = $lesson->platform_id;
+
+        if($platform_id == 1){
+            $patron = '%^ (?:https?://)? (?:www\.)? (?: youtu\.be/ | youtube\.com (?: /embed/ | /v/ | /watch\?v= ) ) ([\w-]{10,12}) $%x';
+            $array = preg_match($patron, $url, $parte);
+            $lesson->iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'. $parte[1] .'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        }else{
+            $patron = '/\/\/(www\.)?vimeo.com\/(\d+)($|\/)/';
+            $array = preg_match($patron, $url, $parte);
+            $lesson->iframe = '<iframe src="https://player.vimeo.com/video/' . $parte[2] . '" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+        }
+    }
+
+    public function updating(Lesson $lesson){
+        $url = $lesson->url; //CAPTURAMOS LA INFORMACION DE LA URL
+        $platform_id = $lesson->platform_id; //CAPTURAMOS LA INFORMACION UPDATE PLATFORM_ID
+
+        // SE LE CONOCCE A ESTE METODO COOMO EXPRESIONES REGULARES
+        if($platform_id == 1){
+            //SI ES 1, EL VALOR SE TRANSFORMA EN EL SIGUIENTE IFRAME
+            $patron = '%^ (?:https?://)? (?:www\.)? (?: youtu\.be/ | youtube\.com (?: /embed/ | /v/ | /watch\?v= ) ) ([\w-]{10,12}) $%x';
+            $array = preg_match($patron, $url, $parte);
+            $lesson->iframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'. $parte[1] .'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        }else{
+            //SI ES 2 SE ACTUALIZA A VIMEO Y ACTUALIZA EL IFRAME
+            $patron = '/\/\/(www\.)?vimeo.com\/(\d+)($|\/)/';
+            $array = preg_match($patron, $url, $parte);
+            $lesson->iframe = '<iframe src="https://player.vimeo.com/video/' . $parte[2] . '" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+        }
+    }
+}
--- resources/views/livewire/instructor/courses-lesson.blade.php
+++ resources/views/livewire/instructor/courses-lesson.blade.php
@@ -1,3 +1,128 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    @foreach ($section->lessons as $item)
+
+        <article class="card mt-4">
+            <div class="card-body">
+                @if ($lesson->id == $item->id)
+                    <form wire:submit.prevent="update">
+
+                        {{-- NOMBRE --}}
+                        <div class="flex items-center">
+                            <label class="w-32">Nombre:</label>
+                            <input wire:model="lesson.name" type="text" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md">
+                        </div>
+                        @error('lesson.name')
+                            <span class="text-xs text-red-500">{{$message}}</span>
+                        @enderror
+
+                        {{-- PLATAFORMAS --}}
+                        <div class="flex items-center mt-4">
+                            <label class="w-32">Plataforma: </label>
+                            <select wire:model="lesson.platform_id" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                @foreach ($platforms as $platform)
+                                    <option value="{{$platform->id}}">{{$platform->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        {{-- URL --}}
+                        <div class="flex items-center mt-4">
+                            <label class="w-32">URL:</label>
+                            <input wire:model="lesson.url" type="text" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md">
+                        </div>
+
+                        @error('lesson.url')
+                            <span class="text-xs text-red-500">{{$message}}</span>
+                        @enderror
+
+                        <div class="mt-4 flex justify-end">
+                            <button type="button" class="btn btn-danger2" wire:click="cancel">Cancelar</button>
+                            <button type="submit" class="btn btn-primary ml-2">Actualizar</button>
+                        </div>
+
+                    </form>
+                @else
+                    <header>
+                        <h1><i class="far fa-play-circle text-indigo-500 mr-1"></i> Lección: {{$item->name}}</h1>
+                    </header>
+
+                    <div>
+                        <hr class="my-2">
+
+                        <p class="text-sm">Plataforma: {{$item->platform->name}}</p>
+                        <p class="text-sm">Enlace: <a class="text-blue-600" href="{{$item->url}}" target="_blank">{{$item->url}}</a></p>
+
+                        <div class="my-2">
+                            <button class="btn btn-primary text-sm" wire:click="edit({{$item}})">Editar</button>
+                            <button class="btn btn-danger2 text-sm" wire:click="destroy({{$item}})">Eliminar</button>
+                        </div>
+
+                        {{-- COMPONENTE LIVE WIRE, INGRESANDO DESCRIPCION --}}
+                        <div class="mb-4">
+                            @livewire('instructor.lesson-description', ['lesson' => $item], key($item->id))
+                        </div>
+
+                        {{-- COMPONENTE LIVE WIRE, RECURSOS --}}
+                        <div>
+                            @livewire('instructor.lesson-resources', ['lesson' => $item], key($item->id))
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </article>
+        
+    @endforeach
+
+    <div class="mt-4" x-data="{open: false}">
+        <a x-show="!open" x-on:click="open = true" class="flex items-center cursor-pointer">
+            <i class="fas fa-plus-square text-2xl text-red-500 mr-2"></i>
+            Agregar nueva Lección
+        </a>
+
+        <article class="card" x-show="open">
+            <div class="card-body">
+                <h1 class="text-xl font-bold mb-4">Agregar Nueva Lección</h1>
+
+                <div class="mb-4">
+                        {{-- NOMBRE --}}
+                        <div class="flex items-center">
+                            <label class="w-32">Nombre:</label>
+                            <input wire:model="name" type="text" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md">
+                        </div>
+                        @error('name')
+                            <span class="text-xs text-red-500">{{$message}}</span>
+                        @enderror
+
+                        {{-- PLATAFORMAS --}}
+                        <div class="flex items-center mt-4">
+                            <label class="w-32">Plataforma: </label>
+                            <select wire:model="platform_id" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                @foreach ($platforms as $platform)
+                                    <option value="{{$platform->id}}">{{$platform->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        {{-- {{$platform_id}} PARA VERIFICAR  --}}
+                        @error('platform_id')
+                            <span class="text-xs text-red-500">{{$message}}</span>
+                        @enderror
+
+                        {{-- URL --}}
+                        <div class="flex items-center mt-4">
+                            <label class="w-32">URL:</label>
+                            <input wire:model="url" type="text" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md">
+                        </div>
+
+                        @error('url')
+                            <span class="text-xs text-red-500">{{$message}}</span>
+                        @enderror
+                </div>
+
+                <div class="flex justify-end">
+                    <button class="btn btn-danger2" x-on:click="open = false">Cancelar</button>
+                    <button class="btn btn-primary ml-2" wire:click="store">Agregar</button>
+                </div>
+            </div>
+        </article>
+    </div>
 </div>
