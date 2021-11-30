3bb4b66a4e4ca41084a1a269099a464ee55c75b4
Pedro <plandauro@unab.edu.pe>
2021-11-30 11:07:03 -0500

    subida Leciones video 42

--- app/Http/Livewire/Instructor/CoursesCurriculum.php
+++ app/Http/Livewire/Instructor/CoursesCurriculum.php
@@ -3,18 +3,60 @@
 namespace App\Http\Livewire\Instructor;
 
 use App\Models\Course;
+use App\Models\Unity;
 use Livewire\Component;
 
 class CoursesCurriculum extends Component
 {
-    public $course;
+    public $course, $section, $name;
+
+    protected $rules = [
+        'section.name' => 'required'
+    ];
 
     public function mount(Course $course){
         $this->course = $course;
+        $this->section = new Unity();
     }
 
     public function render()
     {
         return view('livewire.instructor.courses-curriculum')->layout('layouts.instructor');
     }
+
+    public function store(){
+
+        $this->validate([
+            'name' => 'required'
+        ]);
+
+        Unity::create([
+            'name' => $this->name,
+            'course_id' => $this->course->id
+        ]);
+
+        $this->reset('name');
+        $this->course = Course::find($this->course->id);
+    }
+
+    public function edit(Unity $section){
+        $this->section = $section;
+    }
+
+    public function update(){
+
+        $this->validate();
+
+        $this->section->save();
+        $this->section =  new Unity();
+
+        $this->course = Course::find($this->course->id);
+    }
+
+    public function destroy(Unity $section){
+        $section->delete();
+        $this->course = Course::find($this->course->id);
+    }
+
+
 }
--- resources/views/livewire/instructor/courses-curriculum.blade.php
+++ resources/views/livewire/instructor/courses-curriculum.blade.php
@@ -2,4 +2,64 @@
     <x-slot name="course">
         {{$course->slug}}
     </x-slot>
+
+    <h1 class="text-xl font-bold">LECCIONES DEL CURSO</h1>
+    <hr class="mt-2 mb-6">
+
+    {{-- {{$section}} PARA PROBAR SI MANDA PARAMETROS --}}
+
+    @foreach ($course->unities as $item)
+        <article class="card mb-6">
+            <div class="card-body bg-gray-100">
+                
+                @if ($section->id == $item->id)
+
+                    <form wire:submit.prevent="update">
+                        <input wire:model="section.name" class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" placeholder="Ingrese el nombre de la sección">
+
+                        @error('section.name')
+                            <span class="text-xs text-red-500">{{$message}}</span>
+                        @enderror
+
+                    </form>
+
+                @else
+                    <header class="flex justify-between items-center">
+                        <h1 class="cursor-pointer"><strong>Unidades: </strong>{{$item->name}}</h1>
+                        <div>
+                            <i class="fas fa-edit cursor-pointer text-blue-500" wire:click="edit({{$item}})"></i>
+                            <i class="fas fa-eraser cursor-pointer text-red-500" wire:click="destroy({{$item}})"></i>
+                        </div>
+                    </header>
+                @endif
+
+            </div>
+        </article>
+    @endforeach
+
+    <div x-data="{open: false}">
+        <a x-show="!open" x-on:click="open = true" class="flex items-center cursor-pointer">
+            <i class="fas fa-plus-square text-2xl text-red-500 mr-2"></i>
+            Agregar nueva Seción
+        </a>
+
+        <article class="card" x-show="open">
+            <div class="card-body bg-gray-100">
+                <h1 class="text-xl font-bold mb-4">Agregar Nueva Sección</h1>
+
+                <div class="mb-4">
+                    <input wire:model="name" type="text" class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none shadow" placeholder="Escriba el nombre de la nueva sección">
+                    @error('name')
+                        <span class="text-xs text-red-500">{{$message}}</span>
+                    @enderror
+                </div>
+
+                <div class="flex justify-end">
+                    <button class="btn btn-danger2" x-on:click="open = false">Cancelar</button>
+                    <button class="btn btn-primary ml-2" wire:click="store">Agregar</button>
+                </div>
+            </div>
+        </article>
+    </div>
+
 </div>
