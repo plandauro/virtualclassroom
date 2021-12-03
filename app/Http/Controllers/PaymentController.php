7c82c552839324326e70ca61e464d0e78e039df2
AlexisArce <larce@unab.edu.pe>
2021-12-03 12:32:24 -0500

    updatepay

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -16,7 +16,6 @@ class PaymentController extends Controller
 
     public function pay(Course $course)
     {
-        // After Step 1
         $apiContext = new \PayPal\Rest\ApiContext(
             new \PayPal\Auth\OAuthTokenCredential(
                 config('services.paypal.client_id'),     // ClientID
@@ -24,21 +23,19 @@ class PaymentController extends Controller
             )
         );
 
-        // After Step 2
         $payer = new \PayPal\Api\Payer();
         $payer->setPaymentMethod('paypal');
 
         $amount = new \PayPal\Api\Amount();
-        // $amount->setTotal(number_format($course->price->value, 2, '.', ''));
-        $amount->setTotal($course->price->value); //AQUI SE COLOCA EL VALOR DEL PRECIO SEGUN EL PROYECTO
+        $amount->setTotal($course->price->value);
         $amount->setCurrency('USD');
 
         $transaction = new \PayPal\Api\Transaction();
         $transaction->setAmount($amount);
 
         $redirectUrls = new \PayPal\Api\RedirectUrls();
-        $redirectUrls->setReturnUrl(route('payment.approved', $course))
-            ->setCancelUrl(route('payment.checkout', $course));
+        $redirectUrls->setReturnUrl("https://example.com/your_redirect_url.html")
+            ->setCancelUrl("https://example.com/your_cancel_url.html");
 
         $payment = new \PayPal\Api\Payment();
         $payment->setIntent('sale')
@@ -46,20 +43,23 @@ class PaymentController extends Controller
             ->setTransactions(array($transaction))
             ->setRedirectUrls($redirectUrls);
 
-        // After Step 3
-        try {
-            $payment->create($apiContext);
-
-            return redirect()->away($payment->getApprovalLink());
-            
-        } catch (\PayPal\Exception\PayPalConnectionException $ex) {
-            // This will print the detailed information on the exception.
-            //REALLY HELPFUL FOR DEBUGGING
-            echo $ex->getData();
-        }
+            try {
+                $payment->create($apiContext);
+                
+                return redirect()->away($payment->getApprovalLink());
+                
+                // echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
+            }
+            catch (\PayPal\Exception\PayPalConnectionException $ex) {
+                // This will print the detailed information on the exception.
+                //REALLY HELPFUL FOR DEBUGGING
+                echo $ex->getData();
+            }
     }
+   
 
     public function approved(Request $request, Course $course){
         return $request->all();
     }
+
 }
