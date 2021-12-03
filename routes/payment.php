dc80525d83e109213769d2c516b349e465e52679
Pedro <plandauro@unab.edu.pe>
2021-12-03 12:33:48 -0500

    subida prueba erroes paypal

--- routes/payment.php
+++ routes/payment.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Payment2Controller;
+
 Route::get('{course}/checkout', [PaymentController::class, 'checkout'])->name('checkout');
+
+// pruebita
+Route::get('{course}/checkout2', [Payment2Controller::class, 'checkout2'])->name('checkout2');
 
 Route::get('{course}/pay', [PaymentController::class, 'pay'])->name('pay');
 
